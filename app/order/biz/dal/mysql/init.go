a530d84b235df87a6000fd3c58680071a3cf67fb
ZanmoNG <3062309690@qq.com>
2025-03-04 09:39:29 +0800

    done with order backend

--- app/order/biz/dal/mysql/init.go
+++ app/order/biz/dal/mysql/init.go
@@ -1,7 +1,12 @@
 package mysql
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/cloudwego/biz-demo/gomall/app/order/biz/model"
 	"github.com/cloudwego/biz-demo/gomall/app/order/conf"
+	"github.com/cloudwego/kitex/pkg/klog"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,7 +18,8 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -21,5 +27,11 @@ func Init() {
 	)
 	if err != nil {
 		panic(err)
+	}
+	if os.Getenv("GO_ENV") != "online" {
+		err := DB.AutoMigrate(model.Order{}, &model.OrderItem{})
+		if err != nil {
+			klog.Error(err)
+		}
 	}
 }
